0607df947ecbd20b0fba0ca3ce32833f85e060dd
Corey Light <coreylight@gmail.com>
2015-07-18 15:25:55 -0500

    colors for npm

---
+++ colors.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "primary":"#42A5F5", //$blue-400
+  "success":"#7CB342", //$light-green-600
+  "info":"#9575CD", //$deep-purple-300
+  "warning":"#FFA726", //$orange-400
+  "danger":"#f05223", //custom color
+  "error":"#FFD600" //$yellow-A700
+}
