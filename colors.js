d9bfde294c28abb44e50a98be4ebcded597d799a
Corey Light <coreylight@gmail.com>
2015-08-06 16:36:54 -0500

    colors

--- colors.js
+++ colors.js
@@ -5,4 +5,14 @@ module.exports = {
   "warning":"#FFA726", //$orange-400
   "danger":"#f05223", //custom color
   "error":"#FFD600" //$yellow-A700
+  "gray50":"#fafafa",
+  "gray100":"#f5f5f5",
+  "gray200":"#eeeeee",
+  "gray300":"#e0e0e0",
+  "gray400":"#bdbdbd",
+  "gray500":"#9e9e9e",
+  "gray600":"#757575",
+  "gray700":"#616161",
+  "gray800":"#424242",
+  "gray900":"#212121"
 }
